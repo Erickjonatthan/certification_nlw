55e28478aa17b892e963188f842d7f556fc3907a
Erick Jonathan <zxjonathanxz22@gmail.com>
2024-02-07 22:28:41 -0300

    Add Docker Compose file, PostgreSQL dependency, and StudentController Implement VerifyIfHasCertificationUseCase and related entities

---
+++ src/main/java/com/rocketseat/certification_nlw/modules/students/controllers/StudentController.java
@@ -0,0 +1,31 @@
+package com.rocketseat.certification_nlw.modules.students.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.rocketseat.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import com.rocketseat.certification_nlw.modules.students.useCases.VerifyIfHasCertificationUseCase;
+
+@RestController
+@RequestMapping("/students")
+public class StudentController {
+
+    @Autowired
+    private VerifyIfHasCertificationUseCase verifyIfHasCertificationUseCase;
+
+    @PostMapping("/verifyIfHasCertification")
+    public String verifyIfHasCertification(@RequestBody VerifyHasCertificationDTO verifyHasCertificationDTO) {
+
+        var result = this.verifyIfHasCertificationUseCase.execute(verifyHasCertificationDTO);
+        
+        if(result) {
+           return "The student has the certification";
+        }
+        
+        return "The student does not have the certification";
+    
+    }
+}
